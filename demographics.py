459d228dd9419529132ebaf6740ba5ccecd9f1b3
Kalenga Mumba <157423014+KCM2005@users.noreply.github.com>
2026-03-30 10:34:26 -0400

    Add demographic data analysis in demographics.py

--- demographics.py
+++ demographics.py
@@ -1,1 +1,17 @@
+import pandas as pd
 
+df = pd.read_csv("cleaned_diabetic_data.csv")
+
+#Age summary
+age_summary = df['age'].describe()
+
+#Gender distribution
+gender_counts = df['gender'].value_counts()
+
+#Race distribution
+race_counts = df['race'].value_counts()
+
+#Show summaries
+print(age_summary)
+print(gender_counts)
+print(race_counts)
