5dd70cd55fd3ebd6ad100a6d59c2a049ae190ce8
Kalenga Mumba <157423014+KCM2005@users.noreply.github.com>
2026-03-30 10:33:15 -0400

    Clean diabetic data by dropping specified columns
    Read diabetic data, drop unnecessary columns, and save cleaned data.

--- clean_data.py
+++ clean_data.py
@@ -1,1 +1,19 @@
+import pandas as pd
 
+df = pd.read_csv("diabetic_data.csv")
+
+cols_to_drop = [
+    'weight',
+    'metformin', 'repaglinide', 'nateglinide', 'chlorpropamide',
+    'glimepiride', 'acetohexamide', 'glipizide', 'glyburide', 'tolbutamide',
+    'pioglitazone', 'rosiglitazone', 'acarbose', 'miglitol', 'troglitazone',
+    'tolazamide', 'examide', 'citoglipton', 'glyburide-metformin',
+    'glipizide-metformin', 'glimepiride-pioglitazone', 'metformin-rosiglitazone',
+    'metformin-pioglitazone',
+    'max_glu_serum'
+]
+
+df.drop(columns=cols_to_drop, inplace=True)
+df.reset_index(drop=True, inplace=True)
+
+df.to_csv("cleaned_diabetic_data.csv", index=False)
